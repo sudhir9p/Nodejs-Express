f94a7d266eb472626cb00466e03a72170d89ee53
Sudhir Paturu <sudhir9p@gmail.com>
2019-06-04 18:19:59 +0530

    Initial mongoose setup

--- server.js
+++ server.js
@@ -8,17 +8,12 @@ const express = require('express'),
     app = express(),
     router = express.Router(),
     port = 3000,
-    logfile = fs.createWriteStream(path.join(__dirname, "/apilog.txt"));
+    logfile = fs.createWriteStream(path.join(__dirname, "/apilog.txt")),
+    mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-app.listen(port, () => {
-    console.log(`App listening to ${port}....`)
-    console.log('Press Ctrl+C to quit.')
-});
 
 const routes = new NewsArticlesRoutes(router);
 routes.getRoutes();
@@ -31,5 +26,18 @@ app.use((err, req, res, next) => {
     console.log(err);
     res.status(500).send('Error occured while fetching data');
 });
+
+
+//Mongo Connection 
+mongoose.connect('mongodb://localhost:27017/articledb', { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+    console.log('Connected to MongoDB')
+
+    app.listen(port, function () {
+        console.log('API Server Listening on port ' + port + '!')
+    });
+})
 
 
--- src/models/model.js
+++ src/models/model.js
@@ -1,40 +1,5 @@
 import { articles } from '../data/data.json'
 
 export class NewsArticlesModel {
-    constructor() {
-        this.articles = articles;
-    }
-
-    get() {
-        return this.articles;
-    }
-
-    getById(articleId) {
-        const article = this.articles.find((article) => {
-            if (article.id == articleId)
-                return article;
-        });
-        return article;
-    }
-
-    add(article) {
-        this.articles.push(article);
-    }
-
-    update(articleIndex, article) {
-        this.articles[articleIndex] = article;
-    }
-
-    delete(articleIndex) {
-        this.articles.splice(articleIndex, 1);
-    }
-
-    getArticleIndex(articleId) {
-        const articleIndex = this.articles.findIndex((article, index) => {
-            if (article.id == articleId) {
-                return index;
-            }
-        });
-        return articleIndex;
-    }
+    
 }
